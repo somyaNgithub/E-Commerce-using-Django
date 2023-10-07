ac6f41d0d8ef53e3b4be35c37113908622e2bd1a
Somyanamdeo <somya.official19@gmail.com>
2023-10-07 17:06:13 +0530

    modified

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,10 +118,9 @@ USE_TZ = True
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER='somyanamdeo2401@gmail.com'
-EMAIL_HOST_PASSWORD='vkjydlheefmrizga'
-EMAIL_PORT= '587'
-EMAIL_USE_TSL= True
-EMAIL_USE_SSL= False
+EMAIL_HOST_PASSWORD='xbcfyfzobcutagpx'
+EMAIL_PORT= 587
+EMAIL_USE_TLS= True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 
 
